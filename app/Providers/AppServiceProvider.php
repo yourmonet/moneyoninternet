fd2f621d9555b49143d210f4e72b73a69c6e3083
aldy57 <aldyalfiansyah06@gmail.com>
2026-06-10 23:41:06 +0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         PembayaranKas::observe(PembayaranKasObserver::class);
     }
 }
